eee36ff872f11e44fcd9b8b8710a16b529e6a7cf
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2018-04-06 18:47:29 +0300

    добавил еще один вариант поиска

--- index.js
+++ index.js
@@ -1,26 +1,106 @@
-import streets from './streets';
+/* eslint-disable no-console */
+import streetsMoskow from './moscow-streets';
+import streetsNY from './ny-streets';
 
-const input = document.querySelector('.input');
-const suggestResult = document.querySelector('.suggest');
+const streets = [...streetsMoskow, ...streetsNY];
 
+class Suggest {
+	constructor(parent, handler) {
+		this.parent = parent;
+		this.input = this.parent.querySelector('.input');
+		this.suggestResult = this.parent.querySelector('.suggest');
+		this.handler = handler;
+	}
 
-function inputHandler() {
-	setTimeout(() => {
-		const value = this.value.toLowerCase();
-		let result = [];
-
-		if (value !== '') {
-			for (let adress of streets) {
-
-				if (result.length > 10) break;
-				if (adress.toLowerCase().indexOf(value) !== -1) {
-					result.push(`<div class="suggest__item"><b>${adress}</b></div>`);
-				}
-			}
-		}
-
-		suggestResult.innerHTML = result.join('');
-	}, 500);
+	subscribe() {
+		this.input.addEventListener('keyup', this.handler.bind(null, this.input, this.suggestResult));
+	}
 }
 
-input.addEventListener('keyup', inputHandler);
+const main1 = new Suggest(document.querySelector('.main_1'), suggest_1);
+const main2 = new Suggest(document.querySelector('.main_2'), suggest_2);
+const main3 = new Suggest(document.querySelector('.main_3'), suggest_3);
+
+main1.subscribe();
+main2.subscribe();
+main3.subscribe();
+
+function suggest_1(input, suggestResult) {
+	console.time('time of first version');
+	const value = input.value.toLowerCase();
+	let result = [];
+	let count = 0;
+
+	if (value !== '') {
+		for (let adress of streets) {
+			if (result.length > 10) break;
+			if (adress.toLowerCase().indexOf(value) !== -1) {
+				result.push(`<div class="suggest__item"><b>${adress}</b></div>`);
+			}
+			count++;
+		}
+	}
+
+	console.log('count',count);
+
+	suggestResult.innerHTML = result.join('');
+	console.timeEnd('time of first version');
+}
+
+function suggest_2(input, suggestResult) {
+	const value = input.value;
+	const reg = new RegExp(value,'i') ;
+	let result = [];
+	
+	console.time('time of second version');
+	if (value !== '') {
+		for (let adress of streets) {
+			if (result.length > 10) break;
+			if (reg.test(adress) ) {
+				result.push(`<div class="suggest__item"><b>${adress}</b></div>`);
+			}
+		}		
+	}
+
+	suggestResult.innerHTML = result.join('');
+	console.timeEnd('time of second version');
+}
+
+function suggest_3(input, suggestResult) {
+	const value = input.value;
+	const list = document.getElementById('foundstreets');
+
+	console.time('time of third version');
+	if (value !== '') {
+		if ( list.options ) {
+			var reg = new RegExp(value,'i') ;
+			for (var i = list.options.length-1; i >= 0; i--) {
+				if ( !reg.test(list.options[i].innerHTML ) ) {
+					// возвращаю в исходный массив
+					streets[ list.options[i].id ] = list.options[i].innerHTML;
+					// удаляю option
+					list.removeChild(list.options[i]);
+				}
+			}		
+		}
+
+		// исследую исходный массив
+		for (var i = 0; i < streets.length; i++) {
+			if (list.options.length > 9) break;
+			var item = document.createElement('option');	// создаю новый option в <select id="foundstreets">
+			item.innerHTML = streets[i];
+			if (reg.test(streets[i]) ) {
+				item.id=i;
+				list.appendChild(item.cloneNode(true));	// добавлЯю созданный новый option в <select id="foundstreets">
+				streets[i] = '';
+			}
+		}
+	}
+	console.timeEnd('time of third version');
+}
+
+function suggest_4(input, suggestResult) {
+	const value = input.value;
+
+	
+}
